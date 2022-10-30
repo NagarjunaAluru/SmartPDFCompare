4d0167c1afc9458ad152c5523cd0abffe15a64bd
NagarjunaAluru <54478184+NagarjunaAluru@users.noreply.github.com>
2022-10-30 12:01:24 +0530

    Delete js directory

--- js/controllers/BatchModeCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('batchmodeCtrl', [])
-    .controller('BatchModeCtrl', ['$scope', 'toaster', '$rootScope', '$http',
-        function ($scope, toaster, $rootScope, $http) {
- 
-          
-        }]);
